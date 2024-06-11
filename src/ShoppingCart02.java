38bfc1247ba6db344bcbe400f8fbd2c750f578b5
claudneia <164636272+claudneia@users.noreply.github.com>
2024-06-11 17:14:43 -0300

    Add files via upload

---
+++ src/ShoppingCart02.java
@@ -0,0 +1,27 @@
+public class ShoppingCart02 {
+    public static void main(String[] args) {
+        String custName = "Alex";
+        String itemDesc = "Shirts";
+        String message = custName+" wants to purchase two "+itemDesc;
+        double preco = 650.00;
+        double imposto = 200.00;
+        double quantidade = 0+1;
+        double totalPrice = (preco+imposto)*quantidade;
+        
+        
+        // Declare and initialize numeric fields: price, tax, quantity.   
+        
+        
+        
+        // Declare and assign a calculated totalPrice
+        
+        
+        // Modify message to include quantity 
+        
+        System.out.println(message);
+        System.out.println("O custo total com o imposto é: $"+totalPrice);
+        
+        // Print another message with the total cost
+        
+    }    
+}
